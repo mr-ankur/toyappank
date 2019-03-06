866c560977dac625eda03000544c44e9371a1cbf
ankur1058500 <ankur11.kmr@gmail.com>
2019-03-06 13:19:17 +0530

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  # root "application#hello"
   resources :users
-  #root 'users#index'
-  root 'microposts#index'
+  
+  root 'users#index'
+  #root 'microposts#index'
 
 end
